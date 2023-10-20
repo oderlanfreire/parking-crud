ff73f22b142e55a23be695c8a9b099a79646ab89
Oderlan <oderlanfreire@gmail.com>
2023-10-20 15:52:30 -0300

    conexão com banco de dados criada e exemplo de properties criado

---
+++ src/test/java/com/api/crudestacionamento/CrudEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.crudestacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
