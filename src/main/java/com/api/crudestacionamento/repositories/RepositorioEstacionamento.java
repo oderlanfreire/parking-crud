624bcdc7a098e632e4a7608dcbe23e29c59af068
Oderlan <oderlanfreire@gmail.com>
2023-10-21 15:00:42 -0300

    arquivo de repositorio e arquivo de serviço criados

---
+++ src/main/java/com/api/crudestacionamento/repositories/RepositorioEstacionamento.java
@@ -0,0 +1,7 @@
+package com.api.crudestacionamento.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioEstacionamento extends JpaRepository {
+    
+}
---
+++ src/main/java/com/api/crudestacionamento/services/ServicoEstacionamento.java
@@ -0,0 +1,8 @@
+package com.api.crudestacionamento.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicoEstacionamento {
+    
+}
